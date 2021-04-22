6056ba75b2ce456c3767c8f9ac19b85b2e832c55
Zach Margolis <zachary.margolis@gsa.gov>
2021-04-22 15:18:54 -0700

    Combine OtpSender spec files for 1:1 lib-spec files

--- spec/lib/otp_sender_spec.rb
+++ spec/lib/otp_sender_spec.rb
@@ -1,4 +1,53 @@
-describe Telephony::OtpSender do
+RSpec.describe Telephony::OtpSender do
+  before do
+    Telephony::Test::Message.clear_messages
+    Telephony::Test::Call.clear_calls
+  end
+
+  context 'with the test adapter' do
+    subject { described_class.new(to: to, otp: otp, expiration: expiration, channel: channel) }
+
+    let(:to) { '+1 (202) 262-1234' }
+    let(:otp) { '123456' }
+    let(:expiration) { 5 }
+
+    before do
+      allow(Telephony.config).to receive(:adapter).and_return(:test)
+    end
+
+    context 'for SMS' do
+      let(:channel) { :sms }
+
+      it 'saves the OTP that was sent for authentication' do
+        subject.send_authentication_otp
+
+        expect(Telephony::Test::Message.last_otp).to eq(otp)
+      end
+
+      it 'saves the OTP that was sent for confirmation' do
+        subject.send_confirmation_otp
+
+        expect(Telephony::Test::Message.last_otp).to eq(otp)
+      end
+    end
+
+    context 'for Voice' do
+      let(:channel) { :voice }
+
+      it 'saves the OTP that was sent for authentication' do
+        subject.send_authentication_otp
+
+        expect(Telephony::Test::Call.last_otp).to eq(otp)
+      end
+
+      it 'saves the OTP that was sent for confirmation' do
+        subject.send_confirmation_otp
+
+        expect(Telephony::Test::Call.last_otp).to eq(otp)
+      end
+    end
+  end
+
   context 'with the pinpoint adapter' do
     subject { described_class.new(to: to, otp: otp, expiration: expiration, channel: channel) }
 
